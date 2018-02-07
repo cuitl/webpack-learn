9d2e742948d54a99a644f7cfbcf22ffc61bc7d6f
cuitianliang <cuitionliang@autohome.com.cn>
2018-02-07 16:18:56 +0800

    管理输出

--- base/demo1/src/index.js
+++ base/demo1/src/index.js
@@ -2,6 +2,8 @@ var a = require('./module/a');
 import './css/style.css';
 import icon from './images/image.png';
 
+import printMe from './print';
+
 a.hello();
 
 var app = document.getElementById('app');
@@ -10,3 +12,7 @@ var img = new Image();
 img.src = icon;
 
 app.appendChild(img);
+
+app.addEventListener('click', function() {
+    printMe();
+});
--- base/demo1/webpack.config.js
+++ base/demo1/webpack.config.js
@@ -1,32 +1,43 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-   entry: './src/index.js',
+//    entry: './src/index.js',
+   entry: {
+       index: './src/index.js',
+       print: './src/print.js'
+   },
    output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.bundle.js'
+        // filename: 'index.bundle.js'
+        filename: '[name].bundle.js'
    },
    module: {
-    rules: [
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        },
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-                'file-loader'
-            ]
-        }
-    ]
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
    },
    plugins: [
+       new cleanWebpackPlugin(['dist']),
         // new webpack.optimize.UglifyJsPlugin(),
-        new HtmlWebpackPlugin({template: './src/index.html'})
+        new HtmlWebpackPlugin({
+            // title 属性 在没有 template时使用 才有效果
+            // title: 'Output Manager',
+            template: './src/index.html'
+        })
    ]
 }
